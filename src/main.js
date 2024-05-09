b074bf8a4bdb25405e8f66abc1ac874075377a2c
Andrew Schmidt <andrewschmidt@rocketloans.com>
2024-05-09 10:44:31 -0400

    fix: ran npm run all

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 /**
  * The main function for the action.
@@ -8,21 +8,24 @@ const path = require('path');
  */
 async function run() {
   try {
-    const scope = core.getInput('scope');
-    const registry = core.getInput('registry');
-    const authToken = core.getInput('auth-token');
-    const inputPath = core.getInput('path');
-  
-    const npmrcPath = path.resolve(process.cwd(), inputPath, '.npmrc');
-  
-    console.log(`Writing to ${npmrcPath}`);
-  
-    fs.appendFileSync(npmrcPath, `\n${scope}:registry=${registry}`);
-    fs.appendFileSync(npmrcPath, `\n${registry.replace(/^http(?:s)?:/, '')}/:_authToken=${authToken}`);
-  
-    console.info(`Succesfully wrote to ${npmrcPath}`);
+    const scope = core.getInput('scope')
+    const registry = core.getInput('registry')
+    const authToken = core.getInput('auth-token')
+    const inputPath = core.getInput('path')
+
+    const npmrcPath = path.resolve(process.cwd(), inputPath, '.npmrc')
+
+    console.log(`Writing to ${npmrcPath}`)
+
+    fs.appendFileSync(npmrcPath, `\n${scope}:registry=${registry}`)
+    fs.appendFileSync(
+      npmrcPath,
+      `\n${registry.replace(/^http(?:s)?:/, '')}/:_authToken=${authToken}`
+    )
+
+    console.info(`Succesfully wrote to ${npmrcPath}`)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
